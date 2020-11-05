9d2efe227a8fcf7ebbe7ae06d7bbe2f08b4490fc
Stanislav [shaltai] Pimenov <yarsklad@gmail.com>
2020-11-05 10:47:27 +0300

    Card is ready

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// Find wrapper HTMLElement
+var wrapper = document.querySelector('.article'); // Replace the whole wrapper with its own contents
+
+wrapper.outerHTML = wrapper.innerHTML;
